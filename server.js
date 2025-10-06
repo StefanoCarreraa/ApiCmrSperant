62138a03a61db3c0410f354a87337b19a20e6eee
Stefano Carrera Alvarado <estefano_144@hotmail.com>
2025-10-06 13:02:45 -0500

    CORRECCION

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 import app from "./app.js";
 import { config } from "./config/config.js";
-import listEndpoints from "express-list-endpoints";
 
 // Render te da su propio puerto dinámico
 const PORT = process.env.PORT || config.port || 5007;
 
-console.log("📜 Rutas disponibles en la app:");
-console.log(listEndpoints(app));
 
 // Escucha en todas las interfaces, no solo localhost
 app.listen(PORT, () => {
