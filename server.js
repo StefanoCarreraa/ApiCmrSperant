38c9320237a4b8e43264c9c042f36dede2504e71
Stefano Carrera Alvarado <estefano_144@hotmail.com>
2025-10-06 13:00:02 -0500

    pruebas

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import app from "./app.js";
 import { config } from "./config/config.js";
+import listEndpoints from "express-list-endpoints";
 
 // Render te da su propio puerto dinámico
 const PORT = process.env.PORT || config.port || 5007;
 
+console.log("📜 Rutas disponibles en la app:");
+console.log(listEndpoints(app));
+
 // Escucha en todas las interfaces, no solo localhost
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`🚀 Servidor HTTP corriendo en puerto ${PORT}`);
 });
